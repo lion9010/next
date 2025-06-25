a02321bff89c9b714b95c42515fa42770fb3098c
LeoPalacioM <laion3000@gmail.com>
2025-06-25 14:27:18 -0500

    Pasamos al cap 15, autenticación

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -4,26 +4,45 @@ import { z } from 'zod';
 import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
 import postgres from 'postgres';
+import { error } from 'console';
 
 const sql = postgres(process.env.POSTGRES_URL!, {ssl: 'require'});
 
 const FormSchema = z.object({
     id: z.string(),
-    customerId: z.string(),
-    amount: z.coerce.number(),
-    status: z.enum(['pending', 'paid']),
+    customerId: z.string({invalid_type_error: 'Selecciona un cliente'}),
+    amount: z.coerce.number().gt(0, {message: 'El monto debe ser mayor a 0'}),
+    status: z.enum(['pending', 'paid'], {invalid_type_error: 'Selecciona un estado válido'}),
     date: z.string(),
 }); 
 
 const CreateInvoice = FormSchema.omit({id: true, date: true});
 const UpdateInvoice = FormSchema.omit({ id: true, date: true});
 
-export async function createInvoice(formData: FormData) {
-    const { customerId, amount, status } = CreateInvoice.parse({
+export type State = {
+  errors?: {
+    customerId?: string[];
+    amount?: string[];
+    status?: string[];
+  };
+  message?: string | null;
+};
+
+export async function createInvoice(prevState: State, formData: FormData) {
+    const validatedFields = CreateInvoice.safeParse({
         customerId: formData.get('customerId'),
         amount: formData.get('amount'),
         status: formData.get('status'),
     });
+
+    if (!validatedFields.success) {
+      return {
+        errors: validatedFields.error.flatten().fieldErrors,
+        message: 'Arregla los errores en el formulario.',
+      }
+    };
+
+    const { customerId, amount, status } = validatedFields.data;
     const amountInCents = amount * 100;
     const date = new Date().toISOString().split('T')[0]; // Get the current date in YYYY-MM-DD format
 
@@ -32,9 +51,11 @@ export async function createInvoice(formData: FormData) {
       INSERT INTO invoices (customer_id, amount, status, date)
       VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
     `;
-  } catch (error) {
+  } catch (errors) {
     // We'll log the error to the console for now
-    console.error(error);
+    return{
+      message: 'Error al crear la factura. Por favor revise los datos e inténtalo de nuevo más tarde.',
+    };
   }
 
     revalidatePath('/dashboard/invoices');
