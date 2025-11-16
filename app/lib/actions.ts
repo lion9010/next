6c110b37522958e3511ae5822d788085a92a5288
LeoPalacioM <laion3000@gmail.com>
2025-11-16 14:06:23 -0500

    Corrección de zod

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -12,9 +12,9 @@ const sql = postgres(process.env.POSTGRES_URL!, {ssl: 'require'});
 
 const FormSchema = z.object({
     id: z.string(),
-    customerId: z.string({invalid_type_error: 'Selecciona un cliente'}),
+    customerId: z.string().min(1, 'Selecciona un cliente'),
     amount: z.coerce.number().gt(0, {message: 'El monto debe ser mayor a 0'}),
-    status: z.enum(['pending', 'paid'], {invalid_type_error: 'Selecciona un estado válido'}),
+    status: z.enum(['pending', 'paid'], {message: 'Selecciona un estado válido'}),
     date: z.string(),
 }); 
 
