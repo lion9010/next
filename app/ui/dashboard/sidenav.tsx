6940346f3f1dd07b508094a334625a1f07b76195
LeoPalacioM <laion3000@gmail.com>
2025-12-10 11:05:10 -0500

    Arreglado el salto del switch-theme que en versión movil saltaba al inicio

--- app/ui/dashboard/sidenav.tsx
+++ app/ui/dashboard/sidenav.tsx
@@ -8,17 +8,17 @@ import { ThemeSwitch } from "../utils/theme-switch";
 export default function SideNav() {
   return (
     <div className="flex h-full flex-col px-3 py-4 md:px-2">
+        <div className="mb-2 h-20 w-full text-white flex flex-row justify-between bg-[var(--primary)] rounded-md p-4 md:h-40 md:justify-start">
       <Link
-        className="mb-2 flex h-20 items-end justify-start rounded-md bg-[var(--primary)] p-4 md:h-40"
+        className="flex  items-end"
         href="/"
       >
-        <div className="w-full text-white md:w-40 flex flex-row justify-between">
           <AcmeLogo className="w-32" />
+      </Link>
           <div className="md:hidden">
             <ThemeSwitch />
           </div>
         </div>
-      </Link>
       <div className="flex grow flex-row justify-between space-x-2 md:flex-col md:space-x-0 md:space-y-2">
         <NavLinks />
         <div className="hidden h-auto w-full grow rounded-md bg-[var(--background)] md:block"></div>
