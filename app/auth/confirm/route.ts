aa6418effd5290ce992d0775330517e1e3d48b19
LeoPalacioM <laion3000@gmail.com>
2026-03-29 14:37:58 -0500

    cambie el confirm auth route

--- app/auth/confirm/route.ts
+++ app/auth/confirm/route.ts
@@ -1,14 +1,16 @@
 import { type EmailOtpType } from '@supabase/supabase-js'
-import { type NextRequest } from 'next/server'
-
+import { NextRequest, NextResponse } from 'next/server'
 import { createClient } from '@/app/lib/supabase/server'
-import { redirect } from 'next/navigation'
 
 export async function GET(request: NextRequest) {
   const { searchParams } = new URL(request.url)
+
   const token_hash = searchParams.get('token_hash')
   const type = searchParams.get('type') as EmailOtpType | null
   const next = searchParams.get('next') ?? '/dashboard'
+
+  const redirectTo = request.nextUrl.clone()
+  redirectTo.pathname = next
 
   if (token_hash && type) {
     const supabase = await createClient()
@@ -17,12 +19,12 @@ export async function GET(request: NextRequest) {
       type,
       token_hash,
     })
+
     if (!error) {
-      // redirect user to specified redirect URL or root of app
-      redirect(next)
+      return NextResponse.redirect(redirectTo)
     }
   }
 
-  // redirect the user to an error page with some instructions
-  redirect('/auth/auth-code-error')
+  redirectTo.pathname = '/auth/auth-code-error'
+  return NextResponse.redirect(redirectTo)
 }
